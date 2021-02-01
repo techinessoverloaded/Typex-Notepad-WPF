590b146107b9d164971e06090b0da4e0cbf93073
arun <arunprasadhc@outlook.com>
2021-02-01 19:44:09 +0530

    Commit

--- TypeX Notepad/MainWindow.xaml.cs
+++ TypeX Notepad/MainWindow.xaml.cs
@@ -1,18 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+﻿using System.Windows;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-
 namespace TypeX_Notepad
 {
     /// <summary>
@@ -23,6 +10,30 @@ namespace TypeX_Notepad
         public MainWindow()
         {
             InitializeComponent();
+        }
+        private void New_CanExecute(object sender,CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+        private void New_Executed(object sender,ExecutedRoutedEventArgs e)
+        {
+            MessageBox.Show("New command executed");
+        }
+        private void Open_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+        private void Open_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            MessageBox.Show("Open Command executed");
+        }
+        public static class TypeXCommands
+        {
+            public static readonly RoutedUICommand AutoSave = new RoutedUICommand("AutoSave","AutoSave",typeof(TypeXCommands),
+                new InputGestureCollection()
+                {
+                    new KeyGesture(Key.T,ModifierKeys.Control)
+                });
         }
     }
 }
